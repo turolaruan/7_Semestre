e0e0e090a1df71450467c6b52f3a0fdf731bc513
Ruan P. Turola <113398551+turolaruan@users.noreply.github.com>
2025-02-16 20:22:17 -0300

    Update main.py

--- SISTEMAS_DISTRIBUIDOS/LAB/Lab1_Rest/main.py
+++ SISTEMAS_DISTRIBUIDOS/LAB/Lab1_Rest/main.py
@@ -2,41 +2,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-
-"""
-tarefas = list() 
-
-class Tarefa(BaseModel): # Classe para definir o modelo de uma tarefa
-    tarefa: str
-    prioridade: int
-    feito: bool
-
-@app.get("/") 
-def root():
-    return tarefas
-
-
-@app.get("/tarefa/{pos}") # Retorna a tarefa na posição pos
-def get_tarefa(pos: int):
-    return tarefas[pos]
-
-@app.post("/adicionar/") # Adiciona uma nova tarefa
-def criar_tarefa(tarefa: Tarefa):
-    tarefa.feito = False
-    tarefas.append(tarefa)
-    return len(tarefas)
-
-@app.put("/feito/{pos}") # Marca a tarefa na posição pos como feita
-def marcar_feito(pos: int):
-    tarefas[pos].feito = True
-    return tarefas[pos]
-
-@app.delete("/deletar/{pos}") # Deleta a tarefa na posição pos
-def deletar_tarefa(pos: int):
-    tarefa = tarefas.pop(pos)
-    return tarefa
-"""
-
 lista_contatos = list()
 
 class Contato(BaseModel): # Classe para definir o modelo de um contato
